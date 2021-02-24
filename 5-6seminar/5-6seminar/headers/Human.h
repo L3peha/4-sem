60ed038476d5747e36233621a61a519637c1e17d
L3peha <58639968+L3peha@users.noreply.github.com>
2021-02-25 00:10:48 +0300

    Human

--- 5-6seminar/5-6seminar/headers/Human.h
+++ 5-6seminar/5-6seminar/headers/Human.h
@@ -1,1 +1,19 @@
 #pragma once
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include<iostream>
+
+using namespace std;
+
+class Human
+{
+private:
+	int id;
+	bool alive;
+public:
+	Human();
+	bool getAlive(int i) const;
+};
+
+#endif
---
+++ 5-6seminar/5-6seminar/source/Human.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../headers/Human.h"
+
+using namespace std;
+
+Human::Human()
+{
+	
+}
+
+bool  Human::getAlive(int i) const
+{
+
+}
